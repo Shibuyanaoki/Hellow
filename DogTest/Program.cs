e85b9d668715bbbba999629849549ecfd402bbb9
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2024-10-20 21:24:44 +0900

    プロジェクト ファイルを追加します。

---
+++ DogTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DogTest
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            Dog hachi = new Dog();
+            hachi.Bark();
+
+            Dog pochi = new Dog();
+            pochi.Bark();
+            pochi.Run();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ SR1_Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow C# world!!");
+
+            Console.ReadLine();
+        }
+    }
+}
