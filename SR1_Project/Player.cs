c0c9e479fb9ceb87f4b18502883fed00706c9f81
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2024-10-27 21:48:22 +0900

    完成

---
+++ SR1_Project/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_Project
+{
+    internal class Player
+    {
+        private string name;
+        private int level;
+
+        public Player(string name, int level)
+        {
+            this.name = name;
+            this.level = level;
+        }
+
+        public void Attck()
+        {
+            Console.WriteLine("攻撃!!");
+        }
+
+        public void Defense()
+        {
+            Console.WriteLine("防御した");
+        }
+
+        public void LevelUp()
+        {
+            level++;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetLevel()
+        {
+            return level;
+        }
+
+    }
+}
--- SR1_Project/Program.cs
+++ SR1_Project/Program.cs
@@ -10,7 +10,17 @@ namespace SR1_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow C# world!!");
+
+            Player player = new Player("ロト", 1);
+
+            Console.WriteLine("名前 : {0}",player.GetName());
+            Console.WriteLine("名前 : {0}", player.GetLevel());
+
+            player.Attck();
+            player.Defense();
+
+            player.LevelUp();
+            Console.WriteLine("レベル : {0}",player.GetLevel());
 
             Console.ReadLine();
         }
