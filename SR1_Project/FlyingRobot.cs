67271c9a57378a34e7f3c0538b206335c008c602
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2024-11-11 15:43:31 +0900

    完成

---
+++ SR1_Project/FlyingRobot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_Project
+{
+    internal class FlyingRobot : Robot
+    {
+
+        public FlyingRobot(string name)
+        {
+            this.name = name;
+        }
+
+        public void DropBomd()
+        {
+            if (powerStatus)
+            {
+                Console.WriteLine("{0}は、爆弾を落とした!!", name);
+            }
+            else
+            {
+                Console.WriteLine("{0}は、電源OFFなので爆弾投下は使えません...", name);
+            }
+        }
+    }
+}
---
+++ SR1_Project/TankRobot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_Project
+{
+    internal class TankRobot : Robot
+    {
+        public TankRobot(string name)
+        {
+            this.name = name;
+        }
+
+        public void ShootCannon()
+        {
+            if (powerStatus)
+            {
+                Console.WriteLine("{0}は、キャノン砲を撃った!!", name);
+            }
+            else
+            {
+                Console.WriteLine("{0}は、電源OFFなのでキャノン砲は使えません...", name);
+            }
+        }
+    }
+}
